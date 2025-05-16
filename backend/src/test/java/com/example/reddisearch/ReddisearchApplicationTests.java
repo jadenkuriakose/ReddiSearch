a122031b99e22982c8dffb4f218916e94d852411
jadenkuriakose <jadenjkuriakose@gmail.com>
2025-05-16 13:05:40 -0500

    downloaded base springboot project - reset if need

---
+++ backend/src/test/java/com/example/reddisearch/ReddisearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reddisearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReddisearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
